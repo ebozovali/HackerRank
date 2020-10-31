5b16ab11622de41b8ee4c5d766659da600c90773
ebozovali <ebozovali@users.noreply.github.com>
2020-10-31 23:25:13 +0300

    Add 2D Array - DS challenge

---
+++ InterviewPreparationKit.Test/Array/TwoDArrayTest.cs
@@ -0,0 +1,117 @@
+﻿using InterviewPreperationKit.ArrayEx;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+
+
+
+namespace InterviewPreparationKit.Test.Array
+{
+    public class TwoDArrayTest
+    {
+
+        [Test]
+        public void TestCase0()
+        {
+            //Arrange
+            string fileName = "input00.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Array\Data\TwoDArrayTest\input\", fileName);
+            String input = File.ReadAllText(path);
+            int i = 0, j = 0;
+            int[][] arr = new int[6][];
+
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                arr[i] = new int[6];
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    arr[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            //Act
+            int result = TwoDArray.hourglassSum(arr);
+            ////Assert
+            Assert.AreEqual(19, result);
+         }
+        [Test]
+        public void TestCase1()
+        {
+            //Arrange
+            string fileName = "input01.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Array\Data\TwoDArrayTest\input\", fileName);
+            String input = File.ReadAllText(path);
+            int i = 0, j = 0;
+            int[][] arr = new int[6][];
+
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                arr[i] = new int[6];
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    arr[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            //Act
+            int result = TwoDArray.hourglassSum(arr);
+            ////Assert
+            Assert.AreEqual(13, result);
+        }
+        [Test]
+        public void TestCase8()
+        {
+            //Arrange
+            string fileName = "input08.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Array\Data\TwoDArrayTest\input\", fileName);
+            String input = File.ReadAllText(path);
+            int i = 0, j = 0;
+            int[][] arr = new int[6][];
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                arr[i] = new int[6];
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    arr[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            //Act
+            int result = TwoDArray.hourglassSum(arr);
+            ////Assert
+            Assert.AreEqual(28, result);
+        }
+        [Test]
+        public void TestCase3()
+        {
+            //Arrange
+            string fileName = "input03.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Array\Data\TwoDArrayTest\input\", fileName);
+            String input = File.ReadAllText(path);
+            int i = 0, j = 0;
+            int[][] arr = new int[6][];
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                arr[i] = new int[6];
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    arr[i][j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            //Act
+            int result = TwoDArray.hourglassSum(arr);
+            ////Assert
+            Assert.AreEqual(-6, result);
+        }
+    }
+}
---
+++ InterviewPreperationKit/ArrayEx/TwoDArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreperationKit.ArrayEx
+{
+    public class TwoDArray
+    {
+        //First challenge of arrays
+        // Complete the hourglassSum function below.
+       public static int hourglassSum(int[][] arr)
+        {
+            int largestHourglassSum = 0;
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                for (int j = 0; j < arr[i].Length-2; j++)
+                {
+                    int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                    largestHourglassSum = (i == 0 && j == 0) ? sum :  sum > largestHourglassSum ? sum : largestHourglassSum;
+                }
+            }
+
+            return largestHourglassSum;
+        }
+    }
+}
