389a45cb73a711c864c4fefe568877f92d7eb5ca
ebozovali <ebozovali@users.noreply.github.com>
2020-10-31 13:42:59 +0300

    Add Jumping on the Clouds challenge

---
+++ InterviewPreparationKit.Test/WarmUp/JumpingOnTheCloudsTest.cs
@@ -0,0 +1,37 @@
+﻿using InterviewPreperationKit.WarmUp;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace InterviewPreparationKit.Test.WarmUp
+{
+    public class JumpingOnTheCloudsTest
+    {
+
+        [Test]
+        public void TestCase0()
+        {
+            //Arrange
+           
+            int pathLength = 7;
+            string path = "0 0 1 0 0 1 0";
+            var pathArr=path.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            //Act
+            var result = JumpingOnTheClouds.jumpingOnClouds(pathArr);
+            //Assert
+            Assert.AreEqual(4, result);
+        }
+        [Test]
+        public void TestCase1()
+        {
+            //Arrange
+            int pathLength = 6;
+            string path = "0 0 0 1 0 0";
+            var pathArr = path.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            //Act
+            var result = JumpingOnTheClouds.jumpingOnClouds(pathArr);
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ InterviewPreperationKit/WarmUp/JumpingOnTheClouds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewPreperationKit.WarmUp
+{
+    public class JumpingOnTheClouds
+    {
+        // Complete the jumpingOnClouds function below.
+
+
+        public static int jumpingOnClouds(int[] c)
+        {
+            int minNumberOfJumps = 0;
+
+            for (int i = 1; i < c.Length; i++)
+            {
+                if (i+1 < c.Length &&  c[i + 1] == 0 )
+                {
+                    i++;
+                }
+                if (i != 0)
+                {
+                    minNumberOfJumps++;
+                }
+
+            }
+
+            return minNumberOfJumps;
+        }
+    }
+}
