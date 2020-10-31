11f1a4748cb3ac0c828029e6beaa23c51d4a00ee
ebozovali <ebozovali@users.noreply.github.com>
2020-10-31 14:28:30 +0300

    Add repeated string challenge

---
+++ InterviewPreparationKit.Test/WarmUp/RepeatedStringTest.cs
@@ -0,0 +1,34 @@
+﻿using InterviewPreperationKit.WarmUp;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace InterviewPreparationKit.Test.WarmUp
+{
+    public class RepeatedStringTest
+    {
+
+        [Test]
+        public void TestCase0()
+        {
+            //Arrange
+            long numberOfChars = 10;
+            string s = "aba";
+            //Act
+            var result = RepeatedString.repeatedString(s,numberOfChars);
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+        [Test]
+        public void TestCase1()
+        {
+            //Arrange
+            long numberOfChars = 1000000000000;
+            string s = "a";
+            //Act
+            var result = RepeatedString.repeatedString(s, numberOfChars);
+            //Assert
+            Assert.AreEqual(1000000000000, result);
+        }
+    }
+}
---
+++ InterviewPreperationKit/WarmUp/RepeatedString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewPreperationKit.WarmUp
+{
+   public class RepeatedString
+    {
+
+        // Complete the repeatedString function below.
+        public static long repeatedString(string s, long n)
+        {
+            long numberOfOccurences=0;
+            long k = n / s.Length;
+            long count = 0;
+            count = s.Count(x => x == 'a');
+            numberOfOccurences = count * k;
+            long remainingChars = n % s.Length ;
+            string ss = new String(s.ToCharArray(), 0,(int) remainingChars) ;
+            count = 0;
+            count = ss.Count(x => x == 'a');
+            numberOfOccurences += count;
+
+
+
+            return numberOfOccurences;
+        }
+    }
+}
