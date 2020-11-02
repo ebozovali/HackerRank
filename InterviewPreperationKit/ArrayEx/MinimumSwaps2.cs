8365d3e1a65c34547435242735cf5eb783224cd0
ebozovali <elberbozovali@gmail.com>
2020-11-02 17:18:34 +0300

    Edıt min swap challenge

--- InterviewPreperationKit/ArrayEx/MinimumSwaps2.cs
+++ InterviewPreperationKit/ArrayEx/MinimumSwaps2.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace InterviewPreperationKit.ArrayEx
 {
@@ -8,39 +7,21 @@ namespace InterviewPreperationKit.ArrayEx
         public static int minimumSwaps(int[] arr)
         {
 
+
             int swapNumber = 0, expectedFirst = 1;
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                //if (arr[i] != expectedFirst)
-                //{
-                //    for (int j = i+1; j < arr.Length; j++)
-                //    {
-                //        if (arr[j] == expectedFirst)
-                //        {
-                //            arr[j] = arr[i];
-                //            arr[i] = expectedFirst;
-                //            ++expectedFirst;
-                //            ++swapNumber;
-                //            break;
-                //        }
-                //    }
-                //}
-                //else
-                //{
-                //    ++expectedFirst;
-                //}
+
                 if (arr[i] != expectedFirst)
                 {
-                    int swapIndex = Array.FindIndex(arr, row => row.Equals(expectedFirst));
-
-                   // int swapIndex = Array.FindIndex(arr, x => x.Contains("author"));//Array.IndexOf(arr, expectedFirst);
+                    int swapIndex = Array.IndexOf(arr, expectedFirst);
                     arr[swapIndex] = arr[i];
                     arr[i] = expectedFirst;
                     ++swapNumber;
                 }
                 ++expectedFirst;
             }
-            return 49990;// swapNumber;
+            return swapNumber;// swapNumber;
         }
     }
 }
