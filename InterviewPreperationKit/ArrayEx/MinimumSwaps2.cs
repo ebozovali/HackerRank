e0745a159776e9ea867c7f59e051ba8021233a70
ebozovali <elberbozovali@gmail.com>
2020-11-04 18:07:57 +0300

    Add minimum swap solution

--- InterviewPreperationKit/ArrayEx/MinimumSwaps2.cs
+++ InterviewPreperationKit/ArrayEx/MinimumSwaps2.cs
@@ -6,22 +6,56 @@ namespace InterviewPreperationKit.ArrayEx
     {
         public static int minimumSwaps(int[] arr)
         {
+            int min_index = 0;
+            int min = arr[0];
+            int swaps = 0;
 
-
-            int swapNumber = 0, expectedFirst = 1;
-            for (int i = 0; i < arr.Length - 1; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
-
-                if (arr[i] != expectedFirst)
+                if (arr[i] < min)
                 {
-                    int swapIndex = Array.IndexOf(arr, expectedFirst);
-                    arr[swapIndex] = arr[i];
-                    arr[i] = expectedFirst;
-                    ++swapNumber;
+                    min_index = i;
+                    min = arr[i];
+
                 }
-                ++expectedFirst;
             }
-            return swapNumber;// swapNumber;
+            if (min_index != 0)
+            {
+                int temp = arr[0];
+                arr[0] = arr[min_index];
+                arr[min_index] = temp;
+                swaps++;
+            }
+            for (int cur = 0; cur < arr.Length -1; cur++)
+            {
+                int pos = arr[cur] - arr[0];
+                while (arr[pos] != arr[cur])
+                {
+                    int temp = arr[pos];
+                    arr[pos] = arr[cur];
+                    arr[cur] = temp;
+                    swaps++;
+                    pos = arr[cur] - arr[0];
+                }
+                cur = pos;
+
+            }
+            return swaps;
+
+            //int swapNumber = 0, expectedFirst = 1;
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+
+            //    if (arr[i] != expectedFirst)
+            //    {
+            //        int swapIndex = Array.IndexOf(arr, expectedFirst);
+            //        arr[swapIndex] = arr[i];
+            //        arr[i] = expectedFirst;
+            //        ++swapNumber;
+            //    }
+            //    ++expectedFirst;
+            //}
+            //return swapNumber;// swapNumber;
         }
     }
 }
